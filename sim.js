0761292a421bd1bf199912a6e735380158383eb5
Iggy Glassman <iggyglass@gmail.com>
2022-05-25 12:43:27 -0700

    Added field arrows

--- sim.js
+++ sim.js
@@ -11,16 +11,82 @@ const ShaderSources = {
         gl_Position = vec4(vertPosition, 0.0, 1.0);
     }
     `,
-    potentialFragSource: `
+    fragSource: `
     #extension GL_OES_standard_derivatives : enable
     #define MAX_CHARGES ${maxCharges}
+    #define PI 3.1415927
 
     precision mediump float;
     
     uniform vec2 viewport;
     uniform vec3 charges[MAX_CHARGES];
     uniform int chargesCount;
-    
+
+    const float ARROW_TILE_SIZE = 64.0;
+    const float ARROW_HEAD_ANGLE = 45.0 * PI / 180.0;
+    const float ARROW_HEAD_LENGTH = ARROW_TILE_SIZE / 6.0;
+    const float ARROW_SHAFT_THICKNESS = 3.0;
+
+    float distSquared(vec2 a, vec2 b)
+    {
+        vec2 c = a - b;
+        return dot(c, c);
+    }
+
+    vec2 calcField(vec2 pos)
+    {
+        vec2 field = vec2(0.0);
+
+        for (int i = 0; i < MAX_CHARGES; ++i)
+        {
+            if (i >= chargesCount) break;
+
+            vec2 dir = normalize(pos - charges[i].xy * viewport);
+            field += charges[i].z / distSquared(pos, charges[i].xy * viewport) * dir;
+        }
+
+        return field * 1000000.0;
+    }
+
+    vec2 arrowTileCenterCoord(vec2 pos)
+    {
+        return (floor(pos / ARROW_TILE_SIZE) + 0.5) * ARROW_TILE_SIZE;
+    }
+
+    float arrow(vec2 p, vec2 v)
+    {
+        p -= arrowTileCenterCoord(p);
+
+        float magV = length(v);
+        float magP = length(p);
+
+        if (magV > 0.0)
+        {
+            vec2 dirP = p / magP;
+            vec2 dirV = v / magV;
+
+            magV = clamp(magV, 5.0, ARROW_TILE_SIZE / 2.0);
+            v = dirV * magV;
+
+            float dist = max(
+                // Shaft
+                ARROW_SHAFT_THICKNESS / 4.0 - 
+                    max(abs(dot(p, vec2(dirV.y, -dirV.x))), // Width
+                        abs(dot(p, dirV)) - magV + ARROW_HEAD_LENGTH / 2.0), // Length
+                    
+                    // Arrow head
+                 min(0.0, dot(v - p, dirV) - cos(ARROW_HEAD_ANGLE / 2.0) * length(v - p)) * 2.0 + // Front sides
+                 min(0.0, dot(p, dirV) + ARROW_HEAD_LENGTH - magV)
+            );
+
+            return clamp(1.0 + dist, 0.0, 1.0);
+        }
+        else
+        {
+            return max(0.0, 1.2 - magP);
+        }
+    }
+
     vec4 calcPotentialColor()
     {
         // Calculate potential
@@ -31,54 +97,28 @@ const ShaderSources = {
             if (i >= chargesCount) break;
             potential += charges[i].z / distance(gl_FragCoord.xy, charges[i].xy * viewport);
         }
-    
+
         // Do the coloring
-        float f = fract(potential * 100000.0);
-        float df = fwidth(potential * 100000.0);
-        float a = 1.0 - smoothstep(df * 2.0, df * 2.0, f);
+        float f = fract(potential * 10000.0);
+        float df = fwidth(potential * 10000.0);
+        float a = 1.0 - smoothstep(df * 1.0, df * 2.0, f);
         vec3 color = (potential > 0.0) ? vec3(1.0, 0.3, 0.3) : vec3(0.3, 0.5, 1.0);
 
         return vec4(color, a);
-    }
-
-    vec4 calcFieldColor()
-    {
-        // Calculate field strength
-        float field = 0.0;
-    
-        for (int i = 0; i < MAX_CHARGES; ++i)
-        {
-            if (i >= chargesCount) break;
-            field += charges[i].z / dot(gl_FragCoord.xy, charges[i].xy * viewport);
-        }
-    
-        // Do the coloring
-        float f = fract(field * 100000000.0);
-        float df = fwidth(field * 100000000.0);
-        float a = 1.0 - smoothstep(df * 2.0, df * 2.0, f);
-
-        return vec4(vec3(1.0), a);
     }
 
     void main()
     {
         vec3 color = vec3(0.15);
         vec4 potentialColor = calcPotentialColor();
-        vec4 fieldColor = calcFieldColor();
+        float arrowAlpha = arrow(gl_FragCoord.xy, calcField(arrowTileCenterCoord(gl_FragCoord.xy)) * ARROW_TILE_SIZE * 0.4);
 
+        color = mix(color, vec3(0.5), arrowAlpha * length(calcField(gl_FragCoord.xy)));
         color = mix(color, potentialColor.rgb, potentialColor.a);
         //color = mix(color, fieldColor.rgb, fieldColor.a);
     
         gl_FragColor = vec4(color, 1.0);
     }    
-    `,
-    eFieldFragSource: `
-    precision mediump float;
-
-void main()
-{
-    gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
-}
     `
 }
 
@@ -125,19 +165,25 @@ const unitQuad = [
      1.0,  1.0
 ];
 
+var shaderProgram = {
+    program: null,
+    positionAttribute: null,
+    chargeArrayUniform: null,
+    chargeArrayLengthUniform: null,
+    viewportUniform: null
+}
+
 var posQueue = new ChargeQueue(maxCharges);
 
 // DEBUGGING
-posQueue.add([0.1, 0.2, 0.009]);
-posQueue.add([0.5, 0.3, 0.009]);
+posQueue.add([0.5, 1.0, 0.09]);
+posQueue.add([0.9, 1.0, -0.09]);
 
 function init() {
     if (!gl) {
         console.error('WebGL support is required to run!');
         return;
     }
-
-    document.addEventListener('resize', (_) => initCanvas());
 
     let ext = gl.getExtension('OES_standard_derivatives');
 
@@ -146,75 +192,61 @@ function init() {
         return;
     }
 
-    var vertShader = gl.createShader(gl.VERTEX_SHADER);
-    var potentialFragShader = gl.createShader(gl.FRAGMENT_SHADER);
-    var eFieldFragShader = gl.createShader(gl.FRAGMENT_SHADER);
+    let vertShader = gl.createShader(gl.VERTEX_SHADER);
+    let fragShader = gl.createShader(gl.FRAGMENT_SHADER);
 
     gl.shaderSource(vertShader, ShaderSources.vertSource);
-    gl.shaderSource(potentialFragShader, ShaderSources.potentialFragSource);
-    gl.shaderSource(eFieldFragShader, ShaderSources.eFieldFragSource);
+    gl.shaderSource(fragShader, ShaderSources.fragSource);
 
     gl.compileShader(vertShader);
-    gl.compileShader(potentialFragShader);
-    gl.compileShader(eFieldFragShader);
+    gl.compileShader(fragShader);
 
     if (!gl.getShaderParameter(vertShader, gl.COMPILE_STATUS)) {
         console.error('Could not compile vertex shader!', gl.getShaderInfoLog(vertShader));
         return;
     }
     
-    if (!gl.getShaderParameter(potentialFragShader, gl.COMPILE_STATUS)) {
-        console.error('Could not compile electric potential fragment shader!', gl.getShaderInfoLog(potentialFragShader));
+    if (!gl.getShaderParameter(fragShader, gl.COMPILE_STATUS)) {
+        console.error('Could not compile electric potential fragment shader!', gl.getShaderInfoLog(fragShader));
         return;
     }
 
-    if (!gl.getShaderParameter(eFieldFragShader, gl.COMPILE_STATUS)) {
-        console.error('Could not compile electric field fragment shader!', gl.getShaderInfoLog(eFieldFragShader));
+    shaderProgram.program = gl.createProgram();
+
+    gl.attachShader(shaderProgram.program, vertShader);
+    gl.attachShader(shaderProgram.program, fragShader);
+    gl.linkProgram(shaderProgram.program);
+
+    if (!gl.getProgramParameter(shaderProgram.program, gl.LINK_STATUS)) {
+        console.error('Could not link electric potential program!', gl.getProgramInfoLog(shaderProgram.program));
         return;
     }
 
-    var potentialProgram = gl.createProgram();
+    gl.validateProgram(shaderProgram.program);
 
-    gl.attachShader(potentialProgram, vertShader);
-    gl.attachShader(potentialProgram, potentialFragShader);
-    gl.linkProgram(potentialProgram);
-
-    if (!gl.getProgramParameter(potentialProgram, gl.LINK_STATUS)) {
-        console.error('Could not link electric potential program!', gl.getProgramInfoLog(potentialProgram));
+    if (!gl.getProgramParameter(shaderProgram.program, gl.VALIDATE_STATUS)) {
+        console.error('Error validating program!', gl.getProgramInfoLog(shaderProgram.program));
         return;
     }
 
-    // DEBUG ONLY (validate program)
-    gl.validateProgram(potentialProgram);
-
-    if (!gl.getProgramParameter(potentialProgram, gl.VALIDATE_STATUS)) {
-        console.error('Error validating program!', gl.getProgramInfoLog(potentialProgram));
-        return;
-    }
-    // END DEBUG ONLY
-
-    var vertBuffer = gl.createBuffer();
+    let vertBuffer = gl.createBuffer();
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vertBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(unitQuad), gl.STATIC_DRAW);
 
-    let potentialPosAttrib = gl.getAttribLocation(potentialProgram, 'vertPosition');
-    let potentialPosArrayUniform = gl.getUniformLocation(potentialProgram, 'charges');
-    let potentialPosLenUniform = gl.getUniformLocation(potentialProgram, 'chargesCount');
-    let potentialViewportUniform = gl.getUniformLocation(potentialProgram, 'viewport');
+    shaderProgram.positionAttribute = gl.getAttribLocation(shaderProgram.program, 'vertPosition');
+    shaderProgram.chargeArrayUniform = gl.getUniformLocation(shaderProgram.program, 'charges');
+    shaderProgram.chargeArrayLengthUniform = gl.getUniformLocation(shaderProgram.program, 'chargesCount');
+    shaderProgram.viewportUniform = gl.getUniformLocation(shaderProgram.program, 'viewport');
 
-    gl.vertexAttribPointer(potentialPosAttrib, 2, gl.FLOAT, gl.FALSE, 2 * Float32Array.BYTES_PER_ELEMENT, 0);
-    gl.enableVertexAttribArray(potentialPosAttrib);
+    gl.vertexAttribPointer(shaderProgram.positionAttribute, 2, gl.FLOAT, gl.FALSE, 2 * Float32Array.BYTES_PER_ELEMENT, 0);
+    gl.enableVertexAttribArray(shaderProgram.positionAttribute);
 
-    gl.useProgram(potentialProgram);
-
-    gl.uniform3fv(potentialPosArrayUniform, posQueue.asF32Array());
-    gl.uniform1i(potentialPosLenUniform, posQueue.length());
-    gl.uniform2fv(potentialViewportUniform, new Float32Array([window.innerWidth, window.innerHeight]));
+    gl.useProgram(shaderProgram.program);
 
     initCanvas();
 
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
+    document.addEventListener('resize', (_) => initCanvas());
 }
 
 function initCanvas() {
@@ -224,11 +256,17 @@ function initCanvas() {
     canvas.style.width = `${window.innerWidth}px`;
     canvas.style.height = `${window.innerHeight}px`;
 
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    draw();
 }
 
 function draw() {
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+    gl.uniform3fv(shaderProgram.chargeArrayUniform, posQueue.asF32Array());
+    gl.uniform1i(shaderProgram.chargeArrayLengthUniform, posQueue.length());
+    gl.uniform2fv(shaderProgram.viewportUniform, new Float32Array([window.innerWidth, window.innerHeight]));
+
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
 }
