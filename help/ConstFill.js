f8689ba02ba63ebb403156638f7e73fa11399748
Iggy Glassman <iggyglass@gmail.com>
2023-08-11 17:37:42 -0600

    Help page updates

--- help/ConstFill.js
+++ help/ConstFill.js
@@ -1,10 +1,13 @@
 import * as Consts from '../modules/SimConsts.js';
 
-const maxChargesText = document.getElementById('max-charges');
+const maxChargesElements = document.getElementsByClassName('max-charges');
 const fieldLineNumText = document.getElementById('field-line-num');
 const fieldLineIterText = document.getElementById('field-line-iterations');
 
-maxChargesText.innerHTML = `${Consts.maxCharges}`;
+for (let i = 0; i < maxChargesElements.length; i++) {
+    maxChargesElements[i].innerHTML = `${Consts.maxCharges}`;
+}
+
 fieldLineIterText.innerHTML = `${Consts.fieldLineLength}`;
 fieldLineNumText.innerHTML = `\\(n=${Consts.fieldLineCount}\\)`;
 
