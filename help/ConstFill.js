a7c6bdffd2902d3384f919c20c63e02479444eb8
Iggy Glassman <iggyglass@gmail.com>
2023-08-16 14:45:25 -0600

    Minor changes to help page

--- help/ConstFill.js
+++ help/ConstFill.js
@@ -1,14 +1,18 @@
 import * as Consts from '../modules/SimConsts.js';
 
-const maxChargesElements = document.getElementsByClassName('max-charges');
-const fieldLineNumText = document.getElementById('field-line-num');
-const fieldLineIterText = document.getElementById('field-line-iterations');
+function fillConsts() {
+    const maxChargesElements = document.getElementsByClassName('max-charges');
+    const fieldLineNumText = document.getElementById('field-line-num');
+    const fieldLineIterText = document.getElementById('field-line-iterations');
 
-for (let i = 0; i < maxChargesElements.length; i++) {
-    maxChargesElements[i].innerHTML = `${Consts.maxCharges}`;
+    for (let i = 0; i < maxChargesElements.length; i++) {
+        maxChargesElements[i].innerHTML = `${Consts.maxCharges}`;
+    }
+
+    fieldLineIterText.innerHTML = `${Consts.fieldLineLength}`;
+    fieldLineNumText.innerHTML = `\\(n=${Consts.fieldLineCount}\\)`;
+
+    MathJax.typeset();
 }
 
-fieldLineIterText.innerHTML = `${Consts.fieldLineLength}`;
-fieldLineNumText.innerHTML = `\\(n=${Consts.fieldLineCount}\\)`;
-
-MathJax.typeset();
+window.fillConsts = fillConsts;
